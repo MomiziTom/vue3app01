6c733a1910a305a7311e31c0b7b57c4e1c9824cc
MomiziTom <105560840+MomiziTom@users.noreply.github.com>
2025-03-03 11:32:10 +0900

    textscroll画面サイズで余白出る問題解決

--- src/components/TextScroll.vue
+++ src/components/TextScroll.vue
@@ -1,35 +1,82 @@
 <template>
 	<section class="sec1">
 		<h1 class="sec1__title--first">
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
 		</h1>
 		<h1 class="sec1__title--second">
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
-			Elf Girl Shoot
 		</h1>
 	</section>
 </template>
 
 <script>
+let egs_text = `Elf Girl Shoot
+	Elf Girl Shoot
+	Elf Girl Shoot
+	Elf Girl Shoot
+	Elf Girl Shoot
+	Elf Girl Shoot
+	Elf Girl Shoot
+	Elf Girl Shoot
+`
+let winsize = window.innerWidth;
+const flow_text1 = document.getElementsByClassName("sec1__title--first");
+	const flow_text2 = document.getElementsByClassName("sec1__title--second");
+
+function text_longer(){
+	for(let i = 0 ; i < flow_text1.length; i++){
+		flow_text1[i].textContent = egs_text;
+		flow_text2[i].textContent = egs_text;
+	}
+	winsize = window.innerWidth;
+	if(winsize > 1251){
+		egs_text = `Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+		`
+	}else{
+		egs_text = `Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+			Elf Girl Shoot
+		`
+	}
+
+}	
+
+
 export default {
   name: 'TextScroll',
+  mounted: ()=>{
+	text_longer();
+	window.addEventListener('resize', text_longer);
+  },
+  beforeUnmount: ()=> {
+    window.removeEventListener('resize', text_longer)
+  }
 }
 </script>
 
@@ -45,9 +92,9 @@ section{
 .sec1 {
     width: 100%vw;
     overflow: hidden;
-  }
+}
 
-  .sec1__title--first, .sec1__title--second {
+.sec1__title--first, .sec1__title--second {
     flex-shrink: 0;
 	margin:0;
     width: auto;
@@ -56,15 +103,28 @@ section{
 	text-wrap: nowrap;
     white-space: nowrap;
     font-size: 20px;
-  }
+}
 
-  .sec1__title--first {
-    animation: anim--first 10s infinite linear 0.1s both;
-  }
+@media screen and (min-width:1250px){
+	.sec1__title--first {
+			animation: anim--first 30s infinite linear 0.1s both;
+	}
 
-  .sec1__title--second {
-    animation: anim--second 10s infinite linear 0.1s both;
-  }
+	.sec1__title--second {
+			animation: anim--second 30s infinite linear 0.1s both;
+	}
+}
+
+@media screen and (max-width:1250px){
+	.sec1__title--first {
+			animation: anim--first 10s infinite linear 0.1s both;
+	}
+
+	.sec1__title--second {
+			animation: anim--second 10s infinite linear 0.1s both;
+	}
+}
+
 
 @keyframes anim--first {
   0% {
